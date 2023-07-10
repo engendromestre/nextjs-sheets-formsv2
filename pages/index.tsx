9a6621ad892227939777c83152c6e0d97aec5919
engendromestre <engendro.mestre@gmail.com>
2023-07-10 11:35:34 -0300

    autocomplete off to name, email and number fields

--- pages/index.tsx
+++ pages/index.tsx
@@ -155,6 +155,7 @@ const Home: NextPage = () => {
                     id="name"
                     name="name"
                     placeholder="Digite seu nome completo"
+                    autocomplete="off"
                   />
                   {errors.name && touched.name && (
                     <p className="mt-2 text-sm text-red-600 dark:text-red-500">
@@ -175,6 +176,7 @@ const Home: NextPage = () => {
                     id="email"
                     name="email"
                     placeholder="Digite seu e-mail principal"
+                    autocomplete="off"
                   />
                   {errors.email && touched.email && (
                     <p className="mt-2 text-sm text-red-600 dark:text-red-500">
@@ -198,6 +200,7 @@ const Home: NextPage = () => {
                       ({ field }: { field: any }) => <IMaskInput
                         {...field}
                         type="text"
+                        autocomplete="off"
                         mask={phoneMask}
                         placeholder="Digite seu número de telefone/celular"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"

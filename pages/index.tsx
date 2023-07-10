76772da6d590a8d6786c466686461785b9006ea6
engendromestre <engendro.mestre@gmail.com>
2023-07-10 07:57:03 -0300

    remove credentials gitignore and disable button after click

--- pages/index.tsx
+++ pages/index.tsx
@@ -87,6 +87,7 @@ const Home: NextPage = () => {
   };
 
   const handleSubmit = async (values: MyFormValues, actions: any) => {
+
     values.current_sector = currentSector;
     values.selected_sectors = selectedSectors.toString();
     await axios.post('/api/submit', values)
@@ -140,7 +141,7 @@ const Home: NextPage = () => {
             validationSchema={schema}
             onSubmit={handleSubmit}
           >
-            {({ errors, touched }) => (
+            {({ errors, touched, isSubmitting }) => (
               <Form className="w-80">
                 <div className="mb-12">
                   <label
@@ -306,6 +307,7 @@ const Home: NextPage = () => {
                   {selectedSectors.length == availableSectors && (
                     <button
                       type="submit"
+                      disabled={isSubmitting}
                       className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-2 focus:outline-none 
                   focus:ring-indigo-500 font-medium rounded-lg text-sm px-5 py-2.5 
                   text-center inline-flex items-center dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2"
